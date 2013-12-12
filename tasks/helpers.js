40e473040016af3f60313b87c2a0dccb9b65c2d7
Daniel Cirmaciu <daniel@cirmaciu.cz>
2013-12-12 11:12:34 +0100

    antiCache option added, removed anti-cache scss variable

--- tasks/helpers.js
+++ tasks/helpers.js
@@ -7,7 +7,8 @@ module.exports = function (grunt) {
 	grunt.registerMultiTask('scss_images', 'Create image size variables', function() {
 		var options = this.options({
 			prefix: 'grunt-images',
-			imageRoot: ''
+			imageRoot: '',
+            antiCache: false
 		});
 
 		this.files.forEach(function(filePair) {
@@ -24,9 +25,9 @@ module.exports = function (grunt) {
 			src.forEach(function(file) {
 				var filePath = path.relative(options.imageRoot, file),
 					size = null,
-					time = (new Date(fs.statSync(file).mtime).getTime()/1000).toFixed(0);
+					time = 0;
 
-				if (path.extname(file) === '.svg') {
+				if (path.extname(file) === '.svg') { // waiting for image-size to implement svg support
 					size = {
 						width: 0,
 						height: 0
@@ -34,14 +35,17 @@ module.exports = function (grunt) {
 				} else {
 					size = sizeOf(file);
 				}
+
+                if (options.antiCache) {
+                    time = (new Date(fs.statSync(file).mtime).getTime()/1000).toFixed(0);
+                    filePath = filePath.replace(/([^\.]*)$/, time + '.$1');
+                }
 				list.names.push(filePath);
 				list.widths.push(size.width);
 				list.heights.push(size.height);
-				list.cacheNames.push(filePath.replace(/([^\.]*)$/, time + '.$1'));
 			});
 
 			output += '$' + options.prefix + '-names: ' +  '\'' + list.names.join('\', \'') + '\';\n';
-			output += '$' + options.prefix + '-anti-cache-names: ' +  '\'' + list.cacheNames.join('\', \'') + '\';\n';
 			output += '$' + options.prefix + '-widths: ' + list.widths.join(', ') + ';\n';
 			output += '$' + options.prefix + '-heights: ' + list.heights.join(', ') + ';';
 
